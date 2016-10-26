c03bd7752a9fd1c2f4164650efddbf09fd230492
Daniel Lin <david82213@hotmail.com>
2016-10-25 23:48:29 -0700

    Moved comment section to be within post view so its more like a blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,9 @@ class CommentsController < ApplicationController
     comment_params = params.require(:comment).permit([:title, :description])
     @comment = Comment.new comment_params
     @post = Post.find params[:post_id]
+    @comment.post = @post
     if @comment.save
-      redirect_to post_comment_path(@post,@comment)
+      redirect_to post_path(@post)
     else
       render :new
     end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.order(updated_at: :desc).limit(5)
   end
 
   def about
